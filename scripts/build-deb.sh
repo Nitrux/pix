375483353c21c3ec74baf07e0f5093948acbc666
Uri Herrera <hello@uriherrera.com>
2026-04-02 17:27:05 -0600

    use gh fork

--- scripts/build-deb.sh
+++ scripts/build-deb.sh
@@ -11,7 +11,7 @@ set -e
 
 # -- Download Source
 
-git clone --depth 1 --branch "$PIX_BRANCH" https://invent.kde.org/maui/maui-pix
+git clone --depth 1 --branch "$PIX_BRANCH" https://github.com/Nitrux/maui-pix.git
 
 
 # -- FIX Pix .desktop launcher
